c0c0dcca64099de271f66af94296beb9e8624f61
guole jing <guole.jing@gmail.com>
2022-04-21 18:48:30 +0200

    delete probem with javascript

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,8 +1,11 @@
 class FlatsController < ApplicationController
-  # before_action :set_flat, only: [:]
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
 
   def index
     @flats = Flat.all
+  end
+
+  def show
   end
 
   def new
@@ -10,12 +13,25 @@ class FlatsController < ApplicationController
   end
 
   def create
-    @flat = Flat.new(flat_params)
+    @flat = Flat.create(flat_params)
     if @flat.save
       redirect_to flats_path
     else
       render :new
     end
+  end
+
+  def edit
+  end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flats_path
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
   end
 
   private
@@ -25,6 +41,6 @@ class FlatsController < ApplicationController
   end
 
   def flat_params
-    params.requires(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 end
